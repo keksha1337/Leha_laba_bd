2683269b43fcdc59fcfa6f24f182d30cb3f8dfad
Vaskov Alexey <AVaskov@ad.swtecnn.com>
2018-12-17 16:11:24 +0300

    FileWorker

--- src/sample/FileWorker.java
+++ src/sample/FileWorker.java
@@ -1,14 +1,49 @@
 package sample;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 
 public class FileWorker {
-    public void create(String path) throws IOException {
-        File file = new File(path);
-        if (!file.createNewFile()) {
-            file.delete();
-            file.createNewFile();
+    private String path;
+
+    public FileWorker(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void create() throws IOException {
+        rewrite("");
+    }
+
+    public void write(String str) throws IOException {
+        Files.write(Paths.get(path), str.getBytes(), StandardOpenOption.APPEND);
+    }
+
+    public void rewrite(String str) throws IOException {
+        if (Files.exists(Paths.get(path))) {
+            Files.delete(Paths.get(path));
         }
+        Files.write(Paths.get(path), str.getBytes(), StandardOpenOption.CREATE_NEW);
+    }
+
+    public ArrayList<String> getAllStrings() throws IOException {
+        ArrayList<String> result = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        while (reader.ready()) {
+            result.add(reader.readLine());
+        }
+        return result;
     }
 }
